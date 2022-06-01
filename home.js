665a3d177bea6567a4f2df8a493a815997db4786
ChristopherL <101825137+ChristopherL-creator@users.noreply.github.com>
2022-06-01 11:23:26 +0200

    ss

---
+++ home.js
@@ -0,0 +1,9 @@
+class Home{ 
+    
+    static render(){ 
+        return ` 
+                    <h1>Home</h1> 
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo corporis nisi explicabo nulla sit nostrum assumenda aliquid distinctio vero placeat vel pariatur optio ullam omnis exercitationem molestiae enim, maiores cupiditate?</p>
+                `
+    }
+}
